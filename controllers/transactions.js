b42cc33858f04858bef7c399a848b3384ecdf8ca
Thon Ly <thonly@heartbank.ceo>
2017-02-28 17:00:25 -0800

    Complete transactions

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -5,15 +5,15 @@ module.exports = (heartbank, app) => {
 
     app.get('/transactions', (req, res, next) => {
 
-      const branch = heartbank.transactions(req.cookies.client_id, req.cookies.auth_token, [req.cookies.branch_id, req.cookies.customer_id, req.cookies.user_id]).get({customer:false, q:req.query.q, fetch:req.query.fetch, page:req.query.page, start:req.query.start, end:req.query.end, filters:{account:req.query.account === 'true', fund:req.query.fund == 'true', reserve:req.query.reserve == 'true'}});
+      const branch = heartbank.transactions(req.cookies.client_id, req.cookies.auth_token, [req.cookies.branch_id, req.cookies.customer_id, req.cookies.user_id]).get({customer:false, q:req.query.q, fetch:req.query.fetch, page:req.query.page, start:req.query.start, end:req.query.end, filters:{account:req.query.account === 'on', fund:req.query.fund === 'on', reserve:req.query.reserve === 'on'}});
 
-      const customer = heartbank.transactions(req.cookies.client_id, req.cookies.auth_token, [req.cookies.branch_id, req.cookies.customer_id, req.cookies.user_id]).get({customer:true, q:req.query.q, fetch:req.query.fetch, page:req.query.page, start:req.query.start, end:req.query.end, filters:{account:req.query.account === 'true', fund:req.query.fund == 'true', reserve:req.query.reserve == 'true'}});
+      const customer = heartbank.transactions(req.cookies.client_id, req.cookies.auth_token, [req.cookies.branch_id, req.cookies.customer_id, req.cookies.user_id]).get({customer:true, q:req.query.q, fetch:req.query.fetch, page:req.query.page, start:req.query.start, end:req.query.end, filters:{account:req.query.account === 'on', fund:req.query.fund === 'on', reserve:req.query.reserve === 'on'}});
 
       Promise.all([branch, customer])
       .then(data => {
         if (data[0].code === 200 && data[1].code === 200) {
           //console.log(JSON.stringify(data));
-          res.render('transactions', {branch:data[0].transactions, customer:data[1].transactions});
+          res.render('transactions', {query:req.query, branch:data[0].transactions, customer:data[1].transactions});
         } else {
           res.send(JSON.stringify(data));
         }
@@ -25,10 +25,11 @@ module.exports = (heartbank, app) => {
     });
 
     app.post('/transactions/transaction', (req, res, next) => {
-      heartbank.transactions(req.cookies.client_id, req.cookies.auth_token, [req.cookies.branch_id, req.cookies.customer_id, req.cookies.user_id]).post({command:req.body.command, to:req.body.to, amount:req.body.amount, currency:req.body.currency, anonymity:req.body.anonymity, description:req.body.description, media:req.body.media})
+      heartbank.transactions(req.cookies.client_id, req.cookies.auth_token, [req.cookies.branch_id, req.cookies.customer_id, req.cookies.user_id]).post({command:req.body.command, to:req.body.to, amount:req.body.amount, currency:req.body.currency, anonymity:req.body.anonymity === 'on', description:req.body.description, media:req.file ? fs.readFileSync(path.join(__dirname, '..', req.file.path)) : null})
       .then(data => {
         if (data.code === 200) {
-          console.log(JSON.stringify(data));
+          //console.log(JSON.stringify(data));
+          if (req.file) fs.unlink(path.join(__dirname, '..', req.file.path));
           res.redirect('/transactions');
         } else {
           res.send(JSON.stringify(data));
@@ -44,7 +45,7 @@ module.exports = (heartbank, app) => {
       .then(data => {
         if (data.code === 200) {
           //console.log(JSON.stringify(data));
-          if (req.file) fs.unlinkSync(path.join(__dirname, '..', req.file.path)); //TODO: why does not work??
+          if (req.file) fs.unlink(path.join(__dirname, '..', req.file.path));
           res.redirect('/transactions');
         } else {
           res.send(JSON.stringify(data));
